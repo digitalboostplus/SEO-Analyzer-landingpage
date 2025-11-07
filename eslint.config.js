061dc89b8fadb400a93fcefdd982d1a90de8560d
digitalboostplus <automations@digitalboostplus.io>
2025-11-07 17:38:14 -0600

    Fix ESLint setup and ignore build artifacts

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,43 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tseslint.config(
+export default [
+  {
+    ignores: ['dist/**'],
+  },
   js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    extends: [reactPlugin.configs.recommended],
-    settings: { react: { version: 'detect' } },
-  },
-  {
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
-    },
-  },
-  eslintConfigPrettier,
   {
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
         ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
-  }
-);
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+  eslintConfigPrettier,
+];
